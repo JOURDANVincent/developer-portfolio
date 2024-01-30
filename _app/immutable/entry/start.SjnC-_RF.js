c392bacadf1fd9ae67aefa625ece6f49c4a5ad80
Vins <jourdan.v@orange.fr>
2024-01-30 18:28:34 +0100

    Updates

---
+++ _app/immutable/entry/start.SjnC-_RF.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.zaGFyaRO.js";export{t as start};
--- src/lib/stores/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const expandNav = writable(false)
-
-export const platform = writable('mobile')
-
-export const orientation = writable('landscape')
